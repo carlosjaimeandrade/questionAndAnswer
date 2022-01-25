eff40aeb2997048d1c77982bae82a26f10380937
Carlos jaime <jaime_andrek@hotmail.com>
2022-01-25 18:10:30 -0300

    pegando dado do formulario com body-parser

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = new express();
+const bodyParser = require('body-parser')
 
 //meu motor de html sera o EJS - definindo
 app.set('view engine', 'ejs');
 app.use(express.static('public')); // informando onde será meus arquivos static como css 
+
+app.use(bodyParser.urlencoded({ extended: true })) //configurando o body parser para codificar os dados enviados
+app.use(bodyParser.json()); //configurando para retorno em json trabalhar com epi
 
 app.get('/', (req, res) => {
     res.render('index')
@@ -14,7 +18,9 @@ app.get('/perguntar', (req, res) => {
 })
 
 app.post('/salvarperguntar', (req, res) => {
-    res.send("ola")
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+    res.send(titulo)
 })
 
 app.listen(8580, () => {
