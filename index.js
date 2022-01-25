2c8b942ca6437e191d2b284a65d02d0baac7c196
Carlos jaime <jaime_andrek@hotmail.com>
2022-01-24 21:03:20 -0300

    estrutura de repetição com EJS

--- index.js
+++ index.js
@@ -7,11 +7,17 @@ app.set('view engine', 'ejs');
 app.get('/:nome/:linguagem', (req, res) => {
     //res.send("ola mundo")
     var exibir = true;
+    var produtos = [
+        { nome: "Doritos", preco: 45.5 },
+        { nome: "Doritos1", preco: 45.5 },
+        { nome: "Doritos2", preco: 45.5 }
+    ];
     res.render("index", {
         nome: req.params.nome,
         linguagem: req.params.linguagem,
         empresa: "B&R",
-        exibir: exibir
+        exibir: exibir,
+        produtos: produtos
     })
 })
 
